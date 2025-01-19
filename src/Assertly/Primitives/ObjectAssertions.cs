5edd572d14489f0e574fe8482930331d3926c603
Vikash Chauhan <vikashchauhan51@gmail.com>
2025-01-19 20:34:04 +0530

    completed object implementation

--- src/Assertly/Primitives/ObjectAssertions.cs
+++ src/Assertly/Primitives/ObjectAssertions.cs
@@ -1,34 +1,55 @@
 ﻿using Assertly.Core;
+using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
+using VReflector;
 
 namespace Assertly.Primitives;
 
-public class ObjectAssertions(object? value): ObjectAssertions<object, ObjectAssertions>(value)
+public class ObjectAssertions(object? value) : ObjectAssertions<object, ObjectAssertions>(value)
 {
- 
+
 }
-    public class ObjectAssertions<TSubject, TAssertions>(TSubject? subject) : ReferenceTypeAssertions<TSubject, TAssertions>(subject)
-    where TAssertions : ObjectAssertions<TSubject, TAssertions>
+public class ObjectAssertions<TSubject, TAssertions>(TSubject? subject) : ReferenceTypeAssertions<TSubject, TAssertions>(subject)
+where TAssertions : ObjectAssertions<TSubject, TAssertions>
 {
+    public AndConstraint<TAssertions> Be(TSubject expected,
+        [StringSyntax("CompositeFormat")] string because = "", params object[] becauseArgs)
+    {
+
+        ForCondition(Is.Equal(Subject, expected))
+            .BecauseOf(because, becauseArgs)
+            .FailWith("Expected {context} to be {0}{reason}, but found {1}.", expected, Subject);
+
+        return new AndConstraint<TAssertions>((TAssertions)this);
+    }
+
+    public AndConstraint<TAssertions> NotBe(TSubject expected,
+        [StringSyntax("CompositeFormat")] string because = "", params object[] becauseArgs)
+    {
+
+        ForCondition(!Is.Equal(Subject, expected))
+            .BecauseOf(because, becauseArgs)
+            .FailWith("Expected {context} to be {0}{reason}, but found {1}.", expected, Subject);
+
+        return new AndConstraint<TAssertions>((TAssertions)this);
+    }
     public AndConstraint<TAssertions> BeEquivalentTo(TSubject expected, [StringSyntax("CompositeFormat")] string because = "", params object[] becauseArgs)
     {
-        ForCondition(expected is not null && Subject is not null && expected.Equals(Subject))
+        ForCondition(Is.Same(Subject, expected))
         .BecauseOf(because, becauseArgs)
         .FailWith("Expected {context} to be equivalent to {0} {reason}, but found {1}.", EnsureType(expected), EnsureType(Subject));
 
         return new AndConstraint<TAssertions>((TAssertions)this);
     }
-
     public AndConstraint<TAssertions> NotBeEquivalentTo(TSubject unexpected, [StringSyntax("CompositeFormat")] string because = "", params object[] becauseArgs)
     {
-        ForCondition(Subject is not null && !Subject.Equals(unexpected))
+        ForCondition(!Is.Same(Subject, unexpected))
         .BecauseOf(because, becauseArgs)
         .FailWith("Did not expect {context} to be equivalent to {0} {reason}.", EnsureType(unexpected));
 
         return new AndConstraint<TAssertions>((TAssertions)this);
     }
-
     public AndConstraint<TAssertions> HaveSameHashCode(TSubject other, [StringSyntax("CompositeFormat")] string because = "", params object[] becauseArgs)
     {
         ForCondition(Subject is not null && other is not null && Subject.GetHashCode() == other.GetHashCode())
@@ -37,7 +58,6 @@ public class ObjectAssertions(object? value): ObjectAssertions<object, ObjectAss
 
         return new AndConstraint<TAssertions>((TAssertions)this);
     }
-
     public AndConstraint<TAssertions> NotHaveSameHashCode(TSubject other, [StringSyntax("CompositeFormat")] string because = "", params object[] becauseArgs)
     {
         ForCondition(Subject is not null && other is not null && Subject.GetHashCode() != other.GetHashCode())
@@ -46,7 +66,6 @@ public class ObjectAssertions(object? value): ObjectAssertions<object, ObjectAss
 
         return new AndConstraint<TAssertions>((TAssertions)this);
     }
-
     public AndConstraint<TAssertions> BeDefault([StringSyntax("CompositeFormat")] string because = "", params object[] becauseArgs)
     {
         var defaultValue = default(TSubject);
@@ -56,7 +75,6 @@ public class ObjectAssertions(object? value): ObjectAssertions<object, ObjectAss
 
         return new AndConstraint<TAssertions>((TAssertions)this);
     }
-
     public AndConstraint<TAssertions> NotBeDefault([StringSyntax("CompositeFormat")] string because = "", params object[] becauseArgs)
     {
         var defaultValue = default(TSubject);
@@ -66,7 +84,6 @@ public class ObjectAssertions(object? value): ObjectAssertions<object, ObjectAss
 
         return new AndConstraint<TAssertions>((TAssertions)this);
     }
-
     public AndConstraint<TAssertions> Satisfy(Expression<Func<TSubject, bool>> condition, [StringSyntax("CompositeFormat")] string because = "", params object[] becauseArgs)
     {
         ArgumentNullException.ThrowIfNull(condition);
@@ -74,6 +91,32 @@ public class ObjectAssertions(object? value): ObjectAssertions<object, ObjectAss
         ForCondition(Subject != null && condition.Compile()(Subject))
         .BecauseOf(because, becauseArgs)
         .FailWith("Expected {context:object} to satisfy condition{reason}, but it did not.");
+
+        return new AndConstraint<TAssertions>((TAssertions)this);
+    }
+    public AndConstraint<TAssertions> BeOneOf(params TSubject[] validValues)
+    {
+        return BeOneOf(validValues, string.Empty);
+    }
+    public AndConstraint<TAssertions> BeOneOf(IEnumerable<TSubject> validValues,
+        [StringSyntax("CompositeFormat")] string because = "", params object[] becauseArgs)
+    {
+        ForCondition(validValues.Contains(Subject))
+        .BecauseOf(because, becauseArgs)
+        .FailWith("Expected {context:object} to be one of {0}{reason}, but found {1}.", validValues, Subject);
+
+        return new AndConstraint<TAssertions>((TAssertions)this);
+    }
+    public AndConstraint<TAssertions> BeOneOf(IEnumerable<TSubject> validValues,
+        IEqualityComparer<TSubject> comparer,
+        [StringSyntax("CompositeFormat")] string because = "", params object[] becauseArgs)
+    {
+        ArgumentNullException.ThrowIfNull(validValues);
+        ArgumentNullException.ThrowIfNull(comparer);
+
+        ForCondition(validValues.Contains(Subject, comparer!))
+        .BecauseOf(because, becauseArgs)
+        .FailWith("Expected {context:object} to be one of {0}{reason}, but found {1}.", validValues, Subject);
 
         return new AndConstraint<TAssertions>((TAssertions)this);
     }
